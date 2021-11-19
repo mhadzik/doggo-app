ed62f05bb2e52f35d6ac8ef111fe9139cd81847f
mhadzik <mikolaj.hadzik@gmail.com>
2021-11-19 18:44:47 +0100

    Handle GET call for all dogs from API

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Homepage.scss";
 import { DogCard } from "../DogCard";
+import getAllDogs from "../../utility/Axios";
 
 export const Homepage = () => {
   let arr = [];
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+    getDogs();
+  }, []);
+  console.log(dogs);
+
+  const getDogs = async () => {
+    const dogs = await getAllDogs();
+    if (dogs) {
+      setDogs(dogs);
+    }
+  };
 
   for (let i = 0; i < 10; i++) {
     arr.push(<DogCard />);
---
+++ src/utility/Axios.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const getAllDogs = async () => {
+  try {
+    const res = await axios.get("https://dog.ceo/api/breeds/list/all");
+    if (res) {
+      console.log(res);
+      return res.data.message;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default getAllDogs;
